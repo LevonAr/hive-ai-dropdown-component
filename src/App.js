8d435a0cd370b4c387cbf71841c1dcbdda475c17
LevonAr <levonrm10@gmail.com>
2021-04-21 19:39:17 -0700

    version 1 - Base established. Ugly, some functions working: listOpen, select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Dropdown from "./components/Dropdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      locations: [
+        {
+          id: 0,
+          title: "New York",
+          selected: false,
+          key: "location",
+        },
+        {
+          id: 1,
+          title: "Dublin",
+          selected: false,
+          key: "location",
+        },
+        {
+          id: 2,
+          title: "California",
+          selected: false,
+          key: "location",
+        },
+        {
+          id: 3,
+          title: "Yerevan",
+          selected: false,
+          key: "location",
+        },
+        {
+          id: 4,
+          title: "Izmir",
+          selected: false,
+          key: "location",
+        },
+        {
+          id: 5,
+          title: "Oslo",
+          selected: false,
+          key: "location",
+        },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.tabKeyPressed);
+    window.addEventListener("mousedown", this.mouseClicked);
+  }
+
+  tabKeyPressed = (e) => {
+    if (e.keyCode === 9) {
+      document.querySelector("body").classList.remove("noFocus");
+      window.removeEventListener("keydown", this.tabKeyPressed);
+      window.addEventListener("mousedown", this.mouseClicked);
+    }
+  };
+
+  mouseClicked = () => {
+    document.querySelector("body").classList.add("noFocus");
+    window.removeEventListener("mousedown", this.mouseClicked);
+    window.addEventListener("keydown", this.tabKeyPressed);
+  };
+
+  onChange = (item, name) => {
+    console.log(item, name);
+  };
+
+  render() {
+    const { locations } = this.state;
+
+    return (
+      <div className="App">
+        <p>Dropdown menu examples</p>
+
+        <h3>Regular</h3>
+
+        <div className="wrapper">
+          <Dropdown
+            name="location"
+            title="Select location"
+            list={locations}
+            onChange={this.onChange}
+          />
+        </div>
+
+        <h3>Searchable</h3>
+
+        <div className="wrapper">
+          <Dropdown
+            name="location"
+            searchable={["Search for location", "No matching location"]}
+            title="Select location"
+            list={locations}
+            onChange={this.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
+
+class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    const { title, list } = this.props;
+
+    this.state = {
+      isListOpen: false,
+      headerTitle: this.props.title,
+      selectedItem: null,
+      keyword: "",
+      list,
+    };
+  }
+
+  selectItem = (item) => {
+    const { resetThenSet } = this.props;
+    const { title, id, key } = item;
+
+    this.setState(
+      {
+        headerTitle: title,
+        isListOpen: false,
+      },
+      () => resetThenSet(id, key)
+    );
+  };
+
+  toggleList = () => {
+    this.setState((prevState) => ({
+      isListOpen: !prevState.isListOpen,
+    }));
+  };
+
+  render() {
+    const { isListOpen, headerTitle } = this.state;
+    const { list } = this.props;
+
+    return (
+      <div className="dd-wrapper">
+        <button type="button" className="dd-header" onClick={this.toggleList}>
+          <div className="dd-header-title">{headerTitle}</div>
+          {isListOpen ? (
+            <FontAwesome name="angle-up" size="2x" />
+          ) : (
+            <FontAwesome name="angle-down" size="2x" />
+          )}
+        </button>
+        {isListOpen && (
+          <div role="list" className="dd-list">
+            {list.map((item) => (
+              <button
+                type="button"
+                className="dd-list-item"
+                key={item.id}
+                onClick={() => this.selectItem(item)}
+              >
+                {item.title} {item.selected && <FontAwesome name="check" />}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
